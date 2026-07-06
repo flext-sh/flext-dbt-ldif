522ac4b3e8843b4bdc813d65a0c28a31b950c12a
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-06 00:21:16 -0300

    fixes

--- src/flext_dbt_ldif/services/__init__.py
+++ src/flext_dbt_ldif/services/__init__.py
@@ -8,14 +8,10 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_dbt_ldif.services.client import FlextDbtLdifClient as FlextDbtLdifClient
-    from flext_dbt_ldif.services.core import FlextDbtLdifCore as FlextDbtLdifCore
-    from flext_dbt_ldif.services.service import (
-        FlextDbtLdifServiceMixin as FlextDbtLdifServiceMixin,
-    )
-    from flext_dbt_ldif.services.unified_service import (
-        FlextDbtLdifUnifiedService as FlextDbtLdifUnifiedService,
-    )
+    from flext_dbt_ldif.services.client import FlextDbtLdifClient
+    from flext_dbt_ldif.services.core import FlextDbtLdifCore
+    from flext_dbt_ldif.services.service import FlextDbtLdifServiceMixin
+    from flext_dbt_ldif.services.unified_service import FlextDbtLdifUnifiedService
 _LAZY_IMPORTS = build_lazy_import_map(
     {
         ".client": ("FlextDbtLdifClient",),
