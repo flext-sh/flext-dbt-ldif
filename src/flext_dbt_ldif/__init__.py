aae453da521282f0fc06a4590e08168f4d1c7e96
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-14 14:35:35 -0300

    chore: regenerate from codegen (APPLY exterminated, agent-hook excludes, I001 fix)

--- src/flext_dbt_ldif/__init__.py
+++ src/flext_dbt_ldif/__init__.py
@@ -20,7 +20,8 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_ldif import FlextLdifConstants, d, e, h, r, x
+    from flext_ldif import FlextLdifConstants
+    from flext_meltano import d, e, h, r, x
 
     from . import services
     from ._config import FlextDbtLdifConfig, config
@@ -81,7 +82,8 @@ _LAZY_IMPORTS = MappingProxyType(
             ".services": ("services",),
             ".typings": ("FlextDbtLdifTypes", "t"),
             ".utilities": ("FlextDbtLdifUtilities", "u"),
-            "flext_ldif": ("FlextLdifConstants", "d", "e", "h", "r", "x"),
+            "flext_ldif": ("FlextLdifConstants",),
+            "flext_meltano": ("d", "e", "h", "r", "x"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
